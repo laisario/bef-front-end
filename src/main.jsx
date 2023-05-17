fb862f5fa1ddf5373358e3af7db578c0e9cd829e
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-05-17 16:23:54 -0300

    criei instrument details

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import AuthProvider from './context/Auth'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import InstrumentDetails from './pages/IntrumentDetails';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route Component={Dashboard} path='/' />
           <Route Component={Login} path='/login' />
           <Route Component={Register} path='/register' />
+          <Route Component={InstrumentDetails} path='/:id' />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
---
+++ src/pages/IntrumentDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalibracaoCard from '../components/instruments/cards/CalibracaoCard';
+import { useParams } from 'react-router-dom';
+
+export default function InstrumentCategories() {
+  return (
+
+    <>
+      <CalibracaoCard />
+    </>
+  );
+}
